9c2f23e8e7e679d73668d2538230b624c5d3affe
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-10-20 03:04:14 +0630

    feat: view model added

--- DotNetTodoList/ViewModel/TodoViewModel.cs
+++ DotNetTodoList/ViewModel/TodoViewModel.cs
@@ -2,5 +2,14 @@
 {
     public class TodoViewModel
     {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? CategoryID { get; set; }
+        public int? PriorityLevel { get; set; }
+        public string? Status { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
     }
 }
