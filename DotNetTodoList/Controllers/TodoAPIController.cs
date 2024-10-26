5289e28fdee23f6c46d95eddb3d2e54648196a26
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-10-27 02:17:59 +0630

    feat: todo post

--- DotNetTodoList/Controllers/TodoAPIController.cs
+++ DotNetTodoList/Controllers/TodoAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using DotNetTodoList.DataModel;
 using DotNetTodoList.ViewModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,6 +13,7 @@ namespace DotNetTodoList.Controllers
     public class TodoAPIController : ControllerBase
     {
         private readonly string _connectionString = "Data Source=DESKTOP-KPCHONN\\SQLEXPRESS;Initial Catalog=DotNetToDoList;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+        readonly List<string> status = new List<string> { "Pending", "In Progress", "Completed", "Overdue" };
         [HttpGet]
         public IActionResult GetTodos()
         {
@@ -111,6 +113,108 @@ namespace DotNetTodoList.Controllers
             };
 
             return Ok(todo);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTodo(TodoViewModel todo)
+        {
+            DateTime? createdDate;
+            DateTime? completedDate;
+            string columns = @"[TaskTitle]
+                       ,[TaskDescription]
+                       ,[CategoryID]
+                       ,[PriorityLevel]
+                       ,[Status]
+                       ,[DueDate]";
+
+            string values = @"@title
+                      ,@description
+                      ,@categoryId
+                      ,@level
+                      ,@status
+                      ,@dueDate";
+
+            if (String.IsNullOrEmpty(todo.Title))
+            {
+                return BadRequest("Todo title can't be null");
+            }
+            if (String.IsNullOrEmpty(todo.Description))
+            {
+                return BadRequest("Todo description can't be null");
+            }
+            if (todo.IDCategory == 0 || todo.IDCategory < 0)
+            {
+                return BadRequest("Category Id can't be null or negative");
+            }
+            if (todo.Level == 0 || todo.Level < 0 || todo.Level > 5)
+            {
+                return BadRequest("Priority Level must be between 1 and 5.");
+            }
+            if (String.IsNullOrEmpty(todo.TodoStatus))
+            {
+                return BadRequest("Todo Status can't be null");
+            }
+            else if (!status.Contains(todo.TodoStatus))
+            {
+                return BadRequest("Invalid Status. Status should be one of 'Pending', 'In Progress', 'Completed', 'Overdue'");
+            }
+            if (todo.TodoDueDate == null)
+            {
+                return BadRequest("Due Date can't be null.");
+            }
+            if (todo.TodoCreatedDate != null)
+            {
+                createdDate = todo.TodoCreatedDate;
+                columns += " ,[CreatedDate]";
+                values += " ,@createdDate";
+            }
+            else
+            {
+                createdDate = DateTime.Now;
+            }
+            if (todo.TodoCompletedDate != null)
+            {
+                completedDate = todo.TodoCompletedDate;
+                columns += ", [CompletedDate]";
+                values += " ,@completedDate";
+
+                if (completedDate < createdDate)
+                {
+                    return BadRequest("Completed Date can't be earlier than Created Date");
+                }
+            }
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string query = @$"INSERT INTO [dbo].[ToDoList]
+                           ({columns})
+                     OUTPUT INSERTED.TaskID
+                     VALUES
+                           ({values})";
+
+                SqlCommand cmd = new SqlCommand(query, connection);
+                if (createdDate != null)
+                {
+                    cmd.Parameters.AddWithValue("@createdDate", createdDate);
+                }
+                if (todo.TodoCompletedDate != null)
+                {
+                    cmd.Parameters.AddWithValue("@completedDate", todo.TodoCompletedDate);
+                }
+                cmd.Parameters.AddWithValue("@title", todo.Title);
+                cmd.Parameters.AddWithValue("@description", todo.Description);
+                cmd.Parameters.AddWithValue("@categoryId", todo.IDCategory);
+                cmd.Parameters.AddWithValue("@level", todo.Level);
+                cmd.Parameters.AddWithValue("@status", todo.TodoStatus);
+                cmd.Parameters.AddWithValue("@dueDate", todo.TodoDueDate);
+
+                var insertedId = cmd.ExecuteScalar();
+                connection.Close();
+
+                return Ok($"Your Todo is created with ID: {insertedId}");
+            }
         }
     }
 }
