9f26a8ffb46b47ff0b7acccb388a6874025393d1
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-10-27 23:12:06 +0630

    feat: category GET by ID

--- DotNetTodoList/Controllers/TaskCategoryController.cs
+++ DotNetTodoList/Controllers/TaskCategoryController.cs
@@ -40,5 +40,42 @@ namespace DotNetTodoList.Controllers
 
             return Ok(categories);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategoryById(int id)
+        {
+            CategoryViewModel? category;
+
+            if (id <= 0)
+            {
+                return BadRequest("ID can't be equal or less than 0.");
+            }
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+
+            string query = @"SELECT [CategoryID]
+                              ,[CategoryName]
+                          FROM [dbo].[TaskCategory] WHERE CategoryID = @id";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            cmd.Parameters.AddWithValue("@id", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+
+            connection.Close();
+
+            if (dt.Rows.Count == 0)
+            {
+                return NotFound("Your Requested Category can't be found!");
+            }
+
+            DataRow dr = dt.Rows[0];
+
+            category = new CategoryViewModel { Id = (int)dr["CategoryID"], Name = Convert.ToString(dr["CategoryName"]) };
+
+            return Ok(category);
+        }
     }
 }
